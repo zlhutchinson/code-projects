378b84691196766010c75a5683a2195a7a8caf9c
Zachary <60271950+zlhutchinson@users.noreply.github.com>
2024-06-30 01:59:09 -0500

    Update guiEncoder.py

--- guiEncoder.py
+++ guiEncoder.py
@@ -1,3 +1,9 @@
+## This is a simple encoder/decoder created over the course of 24 hours in an attempt to learn tkinter for gui applications.
+## The program randomly assigns one of four cipher keys to the user and returns their encoded information.
+## They can then return and decode their information by entering their given cipher key and their encoded information.
+## Ideally, the gui interface created through the python library, tkinter, will allow the program to be used with relative ease.
+
+
 import time
 import random
 from tkinter import *
