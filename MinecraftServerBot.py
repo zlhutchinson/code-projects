184e439ab83611cd2f118c94e4ca8169069565d3
Zachary <60271950+zlhutchinson@users.noreply.github.com>
2024-06-30 01:52:01 -0500

    Add files via upload

---
+++ MinecraftServerBot.py
@@ -0,0 +1,32 @@
+import os
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello! If you would like to start the minecraft server type $run. If I don't respond in the future, contact Zachary.")
+
+    if message.content.startswith('$run'):
+        await message.channel.send('Running...')
+        os.chdir(r'C:\Users\Zachary\Desktop\Minecraft Server')
+        os.system('RunServer.bat')
+
+
+token = ""
+
+client.run(token)
